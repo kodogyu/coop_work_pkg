367b869ddbca8030f3a1b993def0be26e692287d
kodogyu <kodogyu4756@gmail.com>
2023-10-09 18:19:25 +0900

    Modify robot spawning position

--- launch/load_coop_robot.launch.py
+++ launch/load_coop_robot.launch.py
@@ -30,7 +30,7 @@ def generate_launch_description():
 
     use_sim_time = LaunchConfiguration('use_sim_time', default='true')
     x_pose = LaunchConfiguration('x_pose', default='-2.0')
-    y_pose = LaunchConfiguration('y_pose', default='-0.5')
+    y_pose = LaunchConfiguration('y_pose', default='-1.0')
 
     robot_state_publisher_cmd = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
