b8f66924d7101a1e2b8221478bd61a976d624651
kodogyu <kodogyu4756@gmail.com>
2023-11-13 14:48:45 +0900

    Edit for multiple image saving

--- coop_work_pkg/drone_image_capture_server.py
+++ coop_work_pkg/drone_image_capture_server.py
@@ -22,33 +22,39 @@ class MinimalService(Node):
                                        'capture_drone_image',
                                        self.drone_image_capture_callback)
 
-        self.br = CvBridge()
-        self.img = np.zeros((800, 480))
+        self.brdge = CvBridge()
+        self.image = np.zeros((800, 480))
         self.image_dir = f"{os.environ['HOME']}/ros2_ws/src/coop_work_pkg/images/"
-        self.image_name = "drone_image.pgm"
+        self.image_name = "drone_image"
+        self.image_number = 0
+        self.image_extension = ".png"
 
     def image_sub_callback(self, msg):
-        current_frame = self.br.imgmsg_to_cv2(msg)
-        self.img = cv2.cvtColor(current_frame, cv2.COLOR_BGR2GRAY)
+        current_frame = self.bridge.imgmsg_to_cv2(msg)
+        self.image = cv2.cvtColor(current_frame, cv2.COLOR_BGR2GRAY)
         self.get_logger().info("image received")
 
-        cv2.imshow("camera", self.img)
+        cv2.imshow("camera", self.image)
         cv2.waitKey(1)
 
     def drone_image_capture_callback(self, request, response):
         # receive the request message
         self.get_logger().info(f'Incoming request\ndata: {request.data}')
-
+        response.message = ""
+        response.success = False
+                
         if request.data:
-            # save an image
-            response.message = os.path.join(self.image_dir, self.image_name)  # full image path
-            response.success = cv2.imwrite(response.message, self.img)
+            # Save an image
+            # full image path
+            response.message = os.path.join(self.image_dir, self.image_name + str(self.image_number) + self.image_extension)
+            response.success = cv2.imwrite(response.message, self.image)
             if response.success:
+                # image saving success
                 self.get_logger().info(f'saved file name: {response.message}')
-        else:
-            response.message = ""
-            response.success = False
-            self.get_logger().info('file saving Failed')
+                self.image_number += 1
+            else:
+                # image saving fail
+                self.get_logger().info('file saving Failed')
 
         self.get_logger().info(f'Request process: {response.success}')
         return response
